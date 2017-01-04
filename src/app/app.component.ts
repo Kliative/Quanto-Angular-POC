514ce000cfeef6ae84e4955b7327cdaa8a177e70
Kli.ative <klish3@gmail.com>
2017-01-04 21:01:51 +0000

    Angular linking

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { User } from './user.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./css/app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  public myForm: FormGroup;
+    public submitted: boolean;
+    public events: any[] = [];
+
+    constructor(private _fb: FormBuilder) { }
+
+    ngOnInit() {
+        
+        this.myForm = this._fb.group({
+            product: '',
+            baseCurrency: '',
+            destinationCurrency: ''
+        });
+
+    }
+
+    save(model: User, isValid: boolean) {
+        this.submitted = true;
+        console.log(model, isValid);
+    }
 }
---
+++ src/app/user.interface.ts
@@ -0,0 +1,9 @@
+export interface User {
+  product: string;
+  baseCurrency: string;
+  destinationCurrency: string;
+  // address?: {
+  //   street?: string;
+  //   postcode?: string;
+  // }
+}
